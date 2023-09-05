f31d09701a36446f2d5ad35ce1c0ed27ee4d30f8
Patryk Pawlina <p4trykpawlina@gmail.com>
2023-09-05 18:00:12 +0200

    route

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import {useState} from 'react'
-import Navbar from "./Navbar.jsx";
-import Database from "./pages/Database.jsx";
-import Catculate from "./pages/Catculate.jsx";
-import Home from "./pages/Home.jsx";
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from './pages/Home.jsx';
+import Database from './pages/Database.jsx';
+import Catculate from './pages/Catculate.jsx';
+import Navbar from './Navbar.jsx';
 
 function App() {
-    let Component
-    switch (window.location.pathname) {
-        case "/":
-            Component = Home
-            break
-        case "/database":
-            Component = Database
-            break
-        case "/catculate":
-            Component = Catculate
-            break
-    }
-
     return (
-        <>
-            <Navbar/>
-            <div className="container">
-                <Component/>
+        <Router>
+            <div>
+                <Navbar />
+                <div className="container">
+                    <Routes>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/database" component={Database} />
+                        <Route path="/catculate" component={Catculate} />
+                    </Routes>
+                </div>
             </div>
-        </>
-    )
+        </Router>
+    );
 }
+
+
+
+
+//return (
+//        <>
+//            <Navbar/>
+//            <div className="container">
+//                <Component/>
+//           </div>
+//        </>
+//    )
+//}
 
 export default App
